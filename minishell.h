ab5ed8afe9208dcffd1f4294bff7ebad1d0c3beb
Leo Boulatrous <lboulatr@z2r7p2.42lyon.fr>
2023-03-20 16:04:40 +0100

    minishell.h and readline test

---
+++ minishell.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lboulatr <lboulatr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 14:31:30 by lboulatr          #+#    #+#             */
+/*   Updated: 2023/03/20 14:33:39 by lboulatr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <dirent.h>
+# include <errno.h>
+# include <string.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+
+#endif
