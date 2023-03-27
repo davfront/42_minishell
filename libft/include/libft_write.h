1cc3719c6badfec9bf41812f690f034d914a8241
davfront <contact@david-pereira.net>
2023-03-27 18:21:39 +0200

    fix libft print functions with no fd arg

--- libft/include/libft_write.h
+++ libft/include/libft_write.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft_write.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dapereir <dapereir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dapereir <dapereir@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 12:50:08 by dapereir          #+#    #+#             */
-/*   Updated: 2022/11/28 10:29:15 by dapereir         ###   ########.fr       */
+/*   Updated: 2023/03/27 17:59:53 by dapereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@
 # include <unistd.h>
 
 void	ft_putchar_fd(char c, int fd);
+void	ft_putchar(char c);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putstr(char *s);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putendl(char *s);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr(int n);
 
 #endif
