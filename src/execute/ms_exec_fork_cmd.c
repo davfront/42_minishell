50d0b8330638e50f86f62df2dc433cb211fd3709
davfront <contact@david-pereira.net>
2023-04-20 11:11:16 +0200

    [#56][fix] command `:`

--- src/execute/ms_exec_fork_cmd.c
+++ src/execute/ms_exec_fork_cmd.c
@@ -34,6 +34,8 @@ void	ms_exec_fork_cmd(t_data *data, t_cmd *cmd)
 {
 	if (!cmd)
 		ms_exit(data, FAILURE);
+	if (ft_streq(cmd->args[0], ":"))
+		ms_exit(data, SUCCESS);
 	if (ms_is_builtin_cmd(cmd->args[0]))
 		ms_exit(data, ms_builtin_cmd(data, cmd->args));
 	if (ms_is_directory(cmd->args[0]))
